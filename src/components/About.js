230cfdc4d0bcfc7d59d17dcf7adb4981088f45a7
Ziru Zou <jiyucoding@yahoo.com>
2023-01-31 21:38:43 -0500

    display and hide <p> based on props.bio

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{(!props.bio) ? null : props.bio}</p>
+      {!props.bio ? null : <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
 
       {/* add your <Links /> component here */}
       <h3>Links</h3>
-      <a href={props.github}>{props.github}</a>
-      <a href={props.linkedin}>{props.linkedin}</a>
+      <a href={props.links.github}>{props.links.github}</a>
+      <a href={props.links.linkedin}>{props.links.linkedin}</a>
     </div>
   );
 }
